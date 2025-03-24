cea88eb287acfef24b8b3e3c44dd6e94245051ec
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-03-24 18:14:19 -0300

    Tratando erros com excecao personalizada

---
+++ Projeto_Xadrez/Tabuleiro/exceptions/TabuleiroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace tabuleiro.exceptions
+{
+    internal class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg) 
+        { 
+        }
+    }
+}
