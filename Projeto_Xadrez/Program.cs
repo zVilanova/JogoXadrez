3e793272e0eafe1f3107d44227b67f91e43ee50d
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-03-24 17:35:46 -0300

    Comecando a implementar o tabuleiro

--- Projeto_Xadrez/Program.cs
+++ Projeto_Xadrez/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Projeto_Xadrez
+﻿using tabuleiro;
+
+namespace Projeto_Xadrez
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
         }
     }
---
+++ Projeto_Xadrez/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas; //Somente o tabuleiro pode mexer nas peças
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
