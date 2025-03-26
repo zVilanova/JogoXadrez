f83dfb8edc2356137714b4de092e458586db640f
Leonardo Guedes <guedesleonardo022@gmail.com>
2025-03-25 21:41:54 -0300

    Jogada especial promocao

--- Projeto_Xadrez/xadrez/PartidaDeXadrez.cs
+++ Projeto_Xadrez/xadrez/PartidaDeXadrez.cs
@@ -143,6 +143,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.Cor);
+                    tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -161,8 +177,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // jogadaespecial en passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
